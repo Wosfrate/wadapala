9f529cce7ac95810e91215ca500738b9a7ef825e
shehan82 <shehansandeepa82@gmail.com>
2021-06-12 20:56:27 +0530

    added login and signup page

---
+++ ui/pages/login.js
@@ -0,0 +1,5 @@
+function login() {
+  return <div className="pt-24 xsm:pt-24 xl:pt-36 pb-5">helloooooooooo</div>;
+}
+
+export default login;
---
+++ ui/pages/signup.js
@@ -0,0 +1,9 @@
+function signup() {
+  return (
+    <div className="pt-24 xsm:pt-24 xl:pt-36 pb-5">
+      <div>fdfdf</div>
+    </div>
+  );
+}
+
+export default signup;
